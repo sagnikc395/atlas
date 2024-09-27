25e73e48bc83509f87cf7b46ae25edd9f474264b
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-09-27 10:48:40 +0530

    src: setup

---
+++ client/src/components/Game.jsx
@@ -0,0 +1,5 @@
+const Game = () => {
+    return <div>ok</div>
+}
+
+export default Game;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,ts,tsx,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
